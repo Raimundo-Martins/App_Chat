63bbd6f94c6228f06f861f314fa890cc9a94ee5b
Raimundo-Martins <cosmomartinsj@gmail.com>
2021-01-26 16:49:13 -0300

    Atualizando class

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:chat/text_composer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -7,6 +11,25 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  Map<String, dynamic> data = {};
+
+  void _sendMessage({String text, File imageFile}) async {
+    if (imageFile != null) {
+      UploadTask task = FirebaseStorage.instance
+          .ref()
+          .child(DateTime.now().millisecondsSinceEpoch.toString())
+          .putFile(imageFile);
+
+      TaskSnapshot snapshot = await task;
+      String url = await snapshot.ref.getDownloadURL();
+      data["imageUrl"] = url;
+    }
+
+    if (text != null) data["texto"] = text;
+
+    FirebaseFirestore.instance.collection("messages").add({"Texto": text});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +37,39 @@ class _ChatScreenState extends State<ChatScreen> {
         title: Text("teste"),
         elevation: 0,
       ),
-      body: TextComposer(),
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream:
+                  FirebaseFirestore.instance.collection("messages").snapshots(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  default:
+                    List<DocumentSnapshot> documents =
+                        snapshot.data.docs.reversed.toList();
+
+                    return ListView.builder(
+                      reverse: true,
+                      itemCount: documents.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(documents[index].data()["texto"]),
+                        );
+                      },
+                    );
+                }
+              },
+            ),
+          ),
+          TextComposer(_sendMessage),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:chat/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
